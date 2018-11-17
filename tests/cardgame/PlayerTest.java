de729f9774a3223023092f1011897dbd5c7190c5
Kamila <kh503@exeter.ac.uk>
2018-11-17 15:55:04 +0000

    Updated PlayerTest

--- tests/cardgame/PlayerTest.java
+++ tests/cardgame/PlayerTest.java
@@ -10,7 +10,30 @@ import static org.junit.Assert.*;
 
 public class PlayerTest {
 
-    Player player = new Player(1);
+    private Player player;
+    private Card card1;
+    private Card card2;
+    private Card card3;
+    private ArrayList<Card> cardArray;
+    private Deck deck1;
+    private Deck deck2;
+
+
+    // before rarther than BeforeClass because we want it ti be executed
+    // before each test and not once before all tests
+    @Before
+    public void setUpClass() throws Exception{
+        player = new Player(1);
+        card1 = new Card(1);
+        card2 = new Card(2);
+        card3 = new Card(3);
+        cardArray = new ArrayList<>();
+        deck1 = new Deck(1);
+        deck2 = new Deck(2);
+        player.playerRounds = 3;
+        CardGame.whoWon = 0;
+        CardGame.endGame = false;
+    }
 
     @Test
     public void testPlayer() {
@@ -20,13 +43,13 @@ public class PlayerTest {
 
     @Test
     public void testSetInitialHand() {
-        Card card = new Card(2);
-        player.setInitialHand(card, card, card, card);
-        ArrayList<Card> cardArray = new ArrayList<>();
-        cardArray.add(card);
-        cardArray.add(card);
-        cardArray.add(card);
-        cardArray.add(card);
+        player.setInitialHand(card2, card2, card2, card2);
+
+        cardArray.add(card2);
+        cardArray.add(card2);
+        cardArray.add(card2);
+        cardArray.add(card2);
+
         Assert.assertEquals(cardArray, player.hand);
     }
 
@@ -34,119 +57,100 @@ public class PlayerTest {
     public void testGetOutputSize() {
        player.output.add("Hello World!");
        player.output.add("Hi World!");
+
        Assert.assertEquals(2, player.getOutputSize());
     }
 
     @Test
     public void testHandToString() {
-        Card card = new Card(3);
-        player.setInitialHand(card, card, card, card);
+        player.setInitialHand(card3, card3, card3, card3);
+
         Assert.assertEquals(" 3 3 3 3", player.handToString());
     }
 
     @Test
     public void testDrawCard() {
-        Deck deck = new Deck(1);
-        Card card1 = new Card(1);
-        Card card2 = new Card(2);
-        deck.setInitialHand(card1, card2, card2, card2);
-        Card drawnCard = player.drawCard(deck);
+        deck1.setInitialHand(card1, card2, card2, card2);
+        Card drawnCard = player.drawCard(deck1);
+
         Assert.assertEquals(1, drawnCard.value);
     }
 
     @Test
     public void testDiscardCard() {
-        Deck deck = new Deck(1);
-        Card card1 = new Card(1);
-        Card card2 = new Card(2);
         player.setInitialHand(card1, card2, card2, card2);
         Card discardedCard = player.hand.remove(0);
-        player.discardCard(discardedCard, deck);
-        Assert.assertEquals(1, discardedCard.value);
-        Assert.assertEquals(1, deck.dHand.get(0).value);
+        player.discardCard(discardedCard, deck1);
+
+        Assert.assertEquals(1, deck1.dHand.get(0).value);
     }
 
     @Test
-    public void testMostRounds() {
-        player.playerRounds = 3;
+    public void testMostRounds5() {
         CardGame.rounds = 5;
         player.mostRounds();
+
         Assert.assertEquals(5, CardGame.rounds);
+    }
+
+    @Test
+    public void testMostRounds3() {
         CardGame.rounds = 1;
         player.mostRounds();
+
         Assert.assertEquals(3, CardGame.rounds);
     }
 
     @Test
     public void testMyActionLeftDeck() {
-        Card card1 = new Card(1);
-        Card card2 = new Card(2);
         player.setInitialHand(card1, card1, card1, card2);
+        deck1.setInitialHand(card1, card1, card1, card1);
+        deck2.setInitialHand(card2, card2, card2, card2);
 
-        Deck leftDeck = new Deck(1);
-        leftDeck.setInitialHand(card1, card1, card1, card1);
+        player.myAction(deck1, deck2);
 
-        Deck rightDeck = new Deck(2);
-        rightDeck.setInitialHand(card2, card2, card2, card2);
-
-        player.myAction(leftDeck, rightDeck);
-
-        ArrayList<Card> cardArray = new ArrayList<>();
         cardArray.add(card1);
         cardArray.add(card1);
         cardArray.add(card1);
 
-        Assert.assertEquals(cardArray, leftDeck.dHand);
+        Assert.assertEquals(cardArray, deck1.dHand);
     }
 
     @Test
     public void testMyActionRightDeck() {
-        Card card1 = new Card(1);
-        Card card2 = new Card(2);
         player.setInitialHand(card1, card1, card1, card2);
+        deck1.setInitialHand(card1, card1, card1, card1);
+        deck2.setInitialHand(card2, card2, card2, card2);
 
-        Deck leftDeck = new Deck(1);
-        leftDeck.setInitialHand(card1, card1, card1, card1);
+        player.myAction(deck1, deck2);
 
-        Deck rightDeck = new Deck(2);
-        rightDeck.setInitialHand(card2, card2, card2, card2);
-
-        player.myAction(leftDeck, rightDeck);
-
-        ArrayList<Card> cardArray = new ArrayList<>();
         cardArray.add(card2);
         cardArray.add(card2);
         cardArray.add(card2);
         cardArray.add(card2);
         cardArray.add(card2);
 
-        Assert.assertEquals(cardArray, rightDeck.dHand);
+        Assert.assertEquals(cardArray, deck2.dHand);
     }
 
     @Test
     public void testMyAction5sameCards() {
-        Card card1 = new Card(1);
         player.setInitialHand(card1, card1, card1, card1);
+        deck1.setInitialHand(card1, card1, card1, card1);
+        deck2.setInitialHand(card1, card1, card1, card1);
 
-        Deck leftDeck = new Deck(1);
-        leftDeck.setInitialHand(card1, card1, card1, card1);
+        player.myAction(deck1, deck2);
 
-        Deck rightDeck = new Deck(2);
-        rightDeck.setInitialHand(card1, card1, card1, card1);
-
-        player.myAction(leftDeck, rightDeck);
-
-        ArrayList<Card> cardArray = new ArrayList<>();
         cardArray.add(card1);
         cardArray.add(card1);
         cardArray.add(card1);
 
-        Assert.assertEquals(cardArray, leftDeck.dHand);
+        Assert.assertEquals(cardArray, deck1.dHand);
 
         cardArray.add(card1);
         cardArray.add(card1);
 
-        Assert.assertEquals(cardArray, rightDeck.dHand);
+        Assert.assertEquals(cardArray, deck2.dHand);
     }
 
     @Test
@@ -163,7 +167,6 @@ public class PlayerTest {
 
     @Test
     public void testCheckHandWon() {
-        Card card1 = new Card(1);
         player.setInitialHand(card1, card1, card1, card1);
         player.checkHand();
         Assert.assertEquals(1, CardGame.whoWon);
@@ -171,11 +174,8 @@ public class PlayerTest {
 
     @Test
     public void testCheckHandNotWon() {
-        Player player = new Player(2);
-        Card card1 = new Card(1);
-        Card card2 = new Card(2);
-        player.setInitialHand(card1, card1, card1, card2);
+        player.setInitialHand(card1, card1, card2, card2);
         player.checkHand();
-        Assert.assertNotEquals(2, CardGame.whoWon);
+        Assert.assertNotEquals(1, CardGame.whoWon);
     }
 }
